8ed9b84bc639b375dfba016c612d67c717a273fd
mike <budge@mikes-mbp.uconnect.utah.edu>
2017-03-27 16:16:36 -0600

    added in class mpi files

--- in_class_mpi/mat_mat_mult.c
+++ in_class_mpi/mat_mat_mult.c
@@ -4,6 +4,25 @@
 
 #define N = 4
 
+// scatter sends adjacent data
+// 	with scatter the send bufer is A,
+// 	send sendCont: (N * N)/P == SendCount
+// 	type MPI_Float or MPI_Double
+// 	destination buffer, local copy of part of A that we are currently working on 
+//		declare myA = (N*N)/P
+//			double myA[N/P][N]
+//	Dest count = (N*N)/P
+//	Dest type should match above
+//	Root = 0
+//	MPI_Comm .. WORLD
+//	double myB[N][N/P] <- transpose 
+
+// isend and ireceive:
+//	proc0 sends its part of A to proc1 and vice versa
+// make sure you don't overwrite stuff
+//	MPI_scatter() -> then MPI_isend() -> then MPI_ireceive() -> then do some calc1() -> mpi_wait() -> then do calc2()
+// with ireceiver, the source is the rank of the proc i'm receiveing from
+
 // for(i = 0; i < N: i++)
 //		for(j = 0; j < N; j++)
 //			c[i][j] = 0
