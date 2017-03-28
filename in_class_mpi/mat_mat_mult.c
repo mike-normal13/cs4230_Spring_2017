26c7b695cbf880a5ca67948479a84b2c7a654dd3
mike <budge@mikes-MacBook-Pro.local>
2017-03-28 13:32:45 -0600

    printing

--- in_class_mpi/mat_mat_mult.c
+++ in_class_mpi/mat_mat_mult.c
@@ -57,7 +57,7 @@ int main(int argc, char* argv[])
 	for(int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
 		{
-			c[i][j] = 0
+			c[i][j] = 0;
 			for(int k = 0; k < N; k++)
 			{
 				c[i][j] += A[i][k] * B[k][j];
