4ea9b24011d7a975305004d7af00667ea1727ce6
mike <budge@mikes-MacBook-Pro.local>
2017-03-28 15:00:03 -0600

    added scatter

--- in_class_mpi/mat_mat_mult.c
+++ in_class_mpi/mat_mat_mult.c
@@ -24,49 +24,48 @@
 //	MPI_scatter() -> then MPI_isend() -> then MPI_ireceive() -> then do some calc1() -> mpi_wait() -> then do calc2()
 // with ireceiver, the source is the rank of the proc i'm receiveing from
 
-// for(i = 0; i < N: i++)
-//		for(j = 0; j < N; j++)
-//			c[i][j] = 0
-//			for(k = 0; k < N; k++)
-//				c[i][j] += A[i][k] * B[k][j];
-
 int main(int argc, char* argv[])
 {
+	MPI_Init(&argc, &argv);
+
 	int rank;
 	int size;
 
-	int c[N][N];
-	int A[N][N];
-	int B[N][N];
+	Double C[N][N];
+	Double A[N][N];
+	Double B[N][N];
 
-	// init data
-	for(int i = 0; i < N; i++)
+	if(rank == 0)
 	{
-		for(int j = 0; j < N; j++)
+		// init data
+		for(int i = 0; i < N; i++)
 		{
-			A[i][j] = i + j;
-			B[i][j] = i * j;
+			for(int j = 0; j < N; j++)
+			{
+				A[i][j] = i + j;
+				B[i][j] = i * j;
+			}
 		}
 	}
-
-	MPI_Init(&argc, &argv);
 
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank );
 	MPI_Comm_size( MPI_COMM_WORLD, &size );
 
+	Double myA[N/size][N];
+
+	MPI_scatter(A, (N*N)/size, MPI_Float, myA, (N*N)/size, MPI_Float, 0, MPI_COMM_WORLD)
+
 	for(int i = 0; i < N; i++)
 		for(int j = 0; j < N; j++)
 		{
-			c[i][j] = 0;
+			C[i][j] = 0;
 			for(int k = 0; k < N; k++)
 			{
-				c[i][j] += A[i][k] * B[k][j];
-				printf("%d ", c[i][j]);
+				C[i][j] += A[i][k] * B[k][j];
+				printf("%d ", C[i][j]);
 			}
 			printf("\n");
 		}
 
-
 	MPI_Finalize();
-
 }
